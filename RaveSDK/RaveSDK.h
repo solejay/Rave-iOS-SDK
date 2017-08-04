d4698674ae0b49cb8730669aa1cbcb3a33341485
Olusegun Solaja <olusegunsolaja@oluseguns-macbook-pro.local>
2017-08-04 17:23:58 +0100

    Initial Commit

---
+++ RaveSDK/RaveSDK.h
@@ -0,0 +1,19 @@
+//
+//  RaveSDK.h
+//  RaveSDK
+//
+//  Created by Olusegun Solaja on 04/08/2017.
+//  Copyright © 2017 Olusegun Solaja. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RaveSDK.
+FOUNDATION_EXPORT double RaveSDKVersionNumber;
+
+//! Project version string for RaveSDK.
+FOUNDATION_EXPORT const unsigned char RaveSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RaveSDK/PublicHeader.h>
+
+
